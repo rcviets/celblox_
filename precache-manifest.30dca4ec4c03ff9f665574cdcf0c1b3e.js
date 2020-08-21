2fe9f4ddcc821dde3d050a1b16a75f7b22f3ad42
Randamus <rcviets@gmail.com>
2020-08-20 20:09:34 -0600

    Updates

---
+++ precache-manifest.30dca4ec4c03ff9f665574cdcf0c1b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c289218467394d838c3b2b39b042c752",
+    "url": "/celblox_/index.html"
+  },
+  {
+    "revision": "db5b959631435041d625",
+    "url": "/celblox_/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a7177f3ee14f9f7668a",
+    "url": "/celblox_/static/js/2.7565541d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/celblox_/static/js/2.7565541d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5b959631435041d625",
+    "url": "/celblox_/static/js/main.0729dafc.chunk.js"
+  },
+  {
+    "revision": "df35343b17e1f283ccd9",
+    "url": "/celblox_/static/js/runtime-main.eed86bcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/celblox_/static/media/logo.5d5d9eef.svg"
+  }
+]);
