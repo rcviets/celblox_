04574a5135fbed0c228e374210b032cfb7fd97de
Randamus <rcviets@gmail.com>
2020-08-25 21:00:22 -0600

    base product card placement

--- src/components/cardProduct/index.js
+++ src/components/cardProduct/index.js
@@ -5,11 +5,46 @@ import "./style.css";
 
 const ProductCard = () => {
     return (
-        <Container>
-            <Row>
+        <Container className="product-card-container">
+            <Row className="product-card-deck">
+                <Col md={12}>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="product-card-deck">
                 <Col md={12}>
                     <Card>
-                        <Card.Body className="product-card">This is some text within a card body.</Card.Body>
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="product-card-deck">
+                <Col md={12}>
+                    <Card>
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="product-card-deck">
+                <Col md={12}>
+                    <Card>
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="product-card-deck">
+                <Col md={12}>
+                    <Card>
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="product-card-deck">
+                <Col md={12}>
+                    <Card>
+                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
                     </Card>
                 </Col>
             </Row>
