032a219b142318967876e496abac967f63ecdf72
Randamus <rcviets@gmail.com>
2020-08-25 21:38:20 -0600

    basic card with image placement

--- src/components/cardProduct/index.js
+++ src/components/cardProduct/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 import { Container, Row, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 import "./style.css";
 
 const ProductCard = () => {
@@ -9,42 +10,60 @@ const ProductCard = () => {
             <Row className="product-card-deck">
                 <Col md={12}>
                     <Card className="product-card-body">
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
                 <Col md={12}>
-                    <Card>
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
                 <Col md={12}>
-                    <Card>
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
                 <Col md={12}>
-                    <Card>
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
                 <Col md={12}>
-                    <Card>
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
                 <Col md={12}>
-                    <Card>
-                        <Card.Body className="product-card-info">This is some text within a card body.</Card.Body>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                                This is some text within a card body.
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
