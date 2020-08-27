433ec8ce6facb63b14d16fc8a56d804391d1a5d7
Randamus <rcviets@gmail.com>
2020-08-26 19:48:58 -0600

    product card styling/positioning

--- src/components/cardProduct/index.js
+++ src/components/cardProduct/index.js
@@ -8,61 +8,55 @@ const ProductCard = () => {
     return (
         <Container className="product-card-container">
             <Row className="product-card-deck">
-                <Col md={12}>
+                <Col md={6}>
                     <Card className="product-card-body">
                         <Card.Body className="product-card-info">
                             <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
+                                Standard 4' Straight Block
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6}>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/singleTaper.png")} thumbnail />
+                            90° Corner Block
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
-                <Col md={12}>
+                <Col md={6}>
                     <Card className="product-card-body">
                         <Card.Body className="product-card-info">
                             <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
+                            45° Corner Block
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6}>
+                    <Card className="product-card-body">
+                        <Card.Body className="product-card-info">
+                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
+                            Single Taper Top Block
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
             <Row className="product-card-deck">
-                <Col md={12}>
+                <Col md={6}>
                     <Card className="product-card-body">
                         <Card.Body className="product-card-info">
                             <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
+                            Double Taper Top Block
                         </Card.Body>
                     </Card>
                 </Col>
-            </Row>
-            <Row className="product-card-deck">
-                <Col md={12}>
+                <Col md={6}>
                     <Card className="product-card-body">
                         <Card.Body className="product-card-info">
                             <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="product-card-deck">
-                <Col md={12}>
-                    <Card className="product-card-body">
-                        <Card.Body className="product-card-info">
-                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="product-card-deck">
-                <Col md={12}>
-                    <Card className="product-card-body">
-                        <Card.Body className="product-card-info">
-                            <Image className="product-image" src={require("../../images/straightBlock.png")} thumbnail />
-                                This is some text within a card body.
+                            Ledge Brick Block   
                         </Card.Body>
                     </Card>
                 </Col>
