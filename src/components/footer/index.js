a6b7caea7544538be62667fe8b11f06eb54c96aa
Randamus <rcviets@gmail.com>
2020-08-30 21:07:46 -0600

    footer info

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,22 @@ import "./style.css";
 const Footer = () => {
     return (
         <Container className="footer" fluid>
-            <p className="footer-text">Celblox</p>
+            <Row>
+                <Col md={3}>
+                    <p className="footer-text">
+                        <b>CelBlox Performance Wall Systems</b><br />
+                        1405 Laukant St.<br />
+                        Reedsburg WI 53959<br />
+                        (608) 616-2015<br />
+                        support@celblox.com
+                    </p>
+                </Col>
+                <Col md={9}>
+                    <p className="footer-copyright">
+                        CelBlox Performance Wall Systems &copy; Copyright 2016 All Rights Reserved
+                    </p>
+                </Col>
+            </Row>
         </Container>
     );
 };
