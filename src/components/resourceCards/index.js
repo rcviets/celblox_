cc115b4e0482a65a41e3c2db9e7f12678478a2fa
Randamus <rcviets@gmail.com>
2020-09-03 21:23:49 -0600

    card text/button styling

--- src/components/resourceCards/index.js
+++ src/components/resourceCards/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import CardColumns from "react-bootstrap/CardColumns";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import "./style.css";
 
 const ResourceCards = () => {
@@ -13,84 +14,91 @@ const ResourceCards = () => {
                     <Card.Body>
                         <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard2.svg")} />
                     <Card.Body>
                         <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard3.svg")} />
                     <Card.Body>
-                    <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
+                        <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard4.svg")} />
                     <Card.Body>
-                    <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
+                        <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard5.svg")} />
                     <Card.Body>
-                    <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
+                        <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard6.svg")} />
                     <Card.Body>
-                    <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
+                        <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src={require("../../images/resourceCard7.svg")} />
                     <Card.Body>
-                    <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
+                        <Card.Title className="resource-card-title">Spec Sheet</Card.Title>
                         <Card.Text className="resource-card-info">
-                            Some text to describe what is being downloaded{' '}
+                            Some text to describe what is being downloaded{' '}<br />
+                            <hr />
+                            <Button className="download-button rounded-0" size="sm">
+                                Download Now
+                            </Button>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
             </CardColumns>
         </Container>
