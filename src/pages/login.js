55dcf152e6eef1954a770b55e5912978fa7e6b7e
Randamus <rcviets@gmail.com>
2020-09-06 11:17:05 -0600

    login page

---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Col } from "react-bootstrap";
+import "./style.css";
+
+class Login extends Component {
+    render() {
+        return(
+            <>
+
+            </>
+        );
+    };
+};
+
+export default Login;
