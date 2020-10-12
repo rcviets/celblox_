bfd876cab938f4f9123178ab4d16cb90978d8990
Randamus <rcviets@gmail.com>
2020-10-12 16:24:29 -0600

    scroll to top page loading component

--- src/App.js
+++ src/App.js
@@ -15,34 +15,37 @@ import Login from "./pages/login";
 import Contact from "./pages/contact";
 import NotFound from "./pages/404";
 import Footer from "./components/footer";
+import ScrollToTop from "./components/scrollToTop";
 import "./App.css";
 
 function App() {
   return (
     <>
-    <NavTop />
-    <Navigation />
+      <NavTop />
+      <Navigation />
       <Router>
-        <Switch>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/resources">
-            <NotFound />
-          </Route>
-          <Route path="/login">
-            <NotFound />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/404">
-            <NotFound />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route path="/products">
+              <Products />
+            </Route>
+            <Route path="/resources">
+              <NotFound />
+            </Route>
+            <Route path="/login">
+              <NotFound />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/404">
+              <NotFound />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </ScrollToTop>
       </Router>
       <Footer />
     </>
