58d21338f1e462218cc22ee4dd82e2d5cf10e1c4
Randamus <rcviets@gmail.com>
2020-10-12 15:42:36 -0600

    form fields final

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import ContactForm from "../components/contactForm/index";
 
 class Contact extends Component {
     render() {
         return (
             <>
-                <Col md={12}>
+                <Row>
+                <Col md={2}></Col>
+                <Col md={8}>
                     <ContactForm />
                 </Col>
+                </Row>
             </>
         );
     };
