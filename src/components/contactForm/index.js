3ec0163a4b9539bb1c73c20673a1c85c177d02be
Randamus <rcviets@gmail.com>
2020-10-12 20:34:19 -0600

    form submit this.state

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,126 +1,144 @@
-import React from "react";
-import { Col } from "react-bootstrap";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./style.css";
 
-const ContactForm = () => {
-    return (
-        <Form className="contact-form">
-            <Form.Row>
-                <Form.Group as={Col} controlId="formGridName">
-                    <Form.Label>Company Name</Form.Label>
-                    <Form.Control type="name" placeholder="Company Name" />
-                </Form.Group>
+class ContactForm extends Component {
+        state = {
+            companyName: "",
+            name: "",
+            email: "",
+            number: "",
+            city: "",
+            state: "",
+            zip: "",
+            distributor: false,
+            installer: false,
+            redirectTo: null
+        };
 
-                <Form.Group as={Col} controlId="formGridName">
-                    <Form.Label>Contact Name</Form.Label>
-                    <Form.Control type="name" placeholder="Name" />
-                </Form.Group>
-            </Form.Row>
 
-            <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="johndoe@company.com" />
-                </Form.Group>
+   /* render() {
+        return (
 
-                <Form.Group as={Col} controlId="formGridNumber">
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="number" placeholder="(123) 456-7890" />
-                </Form.Group>
-            </Form.Row>
+            /*<Form className="contact-form">
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridName">
+                        <Form.Label>Company Name</Form.Label>
+                        <Form.Control type="companyName" placeholder="Company Name" />
+                    </Form.Group>
 
-            <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control />
-                </Form.Group>
+                    <Form.Group as={Col} controlId="formGridName">
+                        <Form.Label>Contact Name</Form.Label>
+                        <Form.Control type="name" placeholder="Name" />
+                    </Form.Group>
+                </Form.Row>
 
-                <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>State</Form.Label>
-                    <Form.Control as="select" defaultValue="Choose...">
-                        <option>Choose...</option>
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
-                        <option value="AS">American Samoa</option>
-                        <option value="GU">Guam</option>
-                        <option value="MP">Northern Mariana Islands</option>
-                        <option value="PR">Puerto Rico</option>
-                        <option value="UM">United States Minor Outlying Islands</option>
-                        <option value="VI">Virgin Islands</option>
-                    </Form.Control>
-                </Form.Group>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="johndoe@company.com" />
+                    </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridZip">
-                    <Form.Label>Zip</Form.Label>
-                    <Form.Control />
-                </Form.Group>
-            </Form.Row>
+                    <Form.Group as={Col} controlId="formGridNumber">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control type="number" placeholder="(123) 456-7890" />
+                    </Form.Group>
+                </Form.Row>
 
-            <Form>
-                {['checkbox'].map((type) => (
-                    <div key={`inline-${type}`} className="mb-3">
-                        <Form.Check inline label="DISTRIBUTOR" type={type} id={`inline-${type}-DISTRIBUTOR`} />
-                        <Form.Check inline label="INSTALLER" type={type} id={`inline-${type}-INSTALLER`} />
-                    </div>
-                ))}
-            </Form>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridCity">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control type="city" />
+                    </Form.Group>
 
-            <Button className="contact-button rounded-0" type="submit">
-                Submit
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>State</Form.Label>
+                        <Form.Control type="state" as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                            <option value="AS">American Samoa</option>
+                            <option value="GU">Guam</option>
+                            <option value="MP">Northern Mariana Islands</option>
+                            <option value="PR">Puerto Rico</option>
+                            <option value="UM">United States Minor Outlying Islands</option>
+                            <option value="VI">Virgin Islands</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridZip">
+                        <Form.Label>Zip</Form.Label>
+                        <Form.Control type="zip" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form>
+                    {['checkbox'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <Form.Check inline label="DISTRIBUTOR" type={type} id={`inline-${type}-DISTRIBUTOR`} />
+                            <Form.Check inline label="INSTALLER" type={type} id={`inline-${type}-INSTALLER`} />
+                        </div>
+                    ))}
+                </Form>
+
+                <Button className="contact-button rounded-0" type="submit">
+                    Submit
             </Button>
-        </Form>
-    );
+            </Form>
+        );
+    };*/
 };
 
 export default ContactForm;
